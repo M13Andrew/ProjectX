9402294911f07094bf54228e2cd4704f343bfe2e
M13Andrew <mayuk.andrew@gmail.com>
2023-05-09 10:38:03 +0300

    New Chages

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello Git')
+from selenium import webdriver
+from selenium.webdriver import Keys
+import time
+from selenium.webdriver.chrome.options import Options #as chrome_options
+#from fake_useragent import UserAgent
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=driver_service)
+driver.maximize_window()
+wait = WebDriverWait(browser, 15, 0.3)
