ba21749e302884f0d2df016541b4cd7d0d451ca9
M13Andrew <mayuk.andrew@gmail.com>
2023-05-09 15:48:06 +0300

    New Chages 1.0

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver import Keys
-import time
-from selenium.webdriver.chrome.options import Options #as chrome_options
-#from fake_useragent import UserAgent
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+age = 25
+name = 'Andrusha'
 
-driver_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=driver_service)
-driver.maximize_window()
-wait = WebDriverWait(browser, 15, 0.3)
+man = {
+    'name': 'Andrusha',
+    'age': '25',
+    'weight': 95
+}
+
