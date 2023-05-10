6e84eb7185537577b99108a1deac05672a5d8b0b
M13Andrew <mayuk.andrew@gmail.com>
2023-05-10 18:31:44 +0300

    New Chages 2.0

--- test_two.py
+++ test_two.py
@@ -1,7 +1,23 @@
 from imports import *
 
 class TestTwo:
+    # def login(self):
+    #     driver_service = Service(ChromeDriverManager().install())
+    #     options = webdriver.ChromeOptions()
+    #     driver = webdriver.Chrome(service=driver_service)
+    #     driver.maximize_window()
+    #     driver.get('https://bes.datawiz.io/login/')
+    #     # wait = WebDriverWait(driver, 15, 0.3)
+    #     # wait.until(ec.visibility_of_element_located((By.ID, "id_auth-username")))
+    #     login = driver.find_element(By.ID, "id_auth-username")
+    #     login.send_keys('andrew.masyuk@datawiz.io')
+    #     password = driver.find_element(By.ID, "id_auth-password")
+    #     password.send_keys('Qweasd2zxc')
+    #     button = driver.find_element(By.XPATH, "//button[@class='btn btn-primary btn-block']")
+    #     button.click()
+
     def test_three(self):
+        # self.login()
         driver_service = Service(ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
         driver = webdriver.Chrome(service=driver_service)
@@ -29,10 +45,16 @@ class TestTwo:
         driver.switch_to.window(driver.window_handles[1])
         # time.sleep(5)
 
-        wait.until(ec.presence_of_element_located((By.XPATH, '//div[@class="ant-tabs-tab"][3]')))
-        button_n = driver.find_element(By.XPATH, '//div[@class="ant-tabs-tab"][3]')
+        button_n = wait.until(ec.presence_of_element_located((By.XPATH, '//div[@class="ant-tabs-tab"][3]')))
+        # wait.until(ec.presence_of_element_located((By.XPATH, '//div[@class="ant-tabs-tab"][3]')))
+        # button_n = driver.find_element(By.XPATH, '//div[@class="ant-tabs-tab"][3]')
         button_n.click()
+
+        plan = wai
+
         time.sleep(5)
+
+
 
 
 
