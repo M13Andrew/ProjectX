094db7057c3a138c46054656961f81cee5f6e901
M13Andrew <mayuk.andrew@gmail.com>
2023-05-11 17:46:31 +0300

    New Chages day 3

--- main.py
+++
@@ -1,9 +0,0 @@
-age = 25
-name = 'Andrusha'
-
-man = {
-    'name': 'Andrusha',
-    'age': '25',
-    'weight': 95
-}
-
--- test_two.py
+++ test_two.py
@@ -1,19 +1,19 @@
 from imports import *
 
 class TestTwo:
-    def login(self):
-        driver_service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=driver_service)
-        driver.maximize_window()
-        driver.get('https://bes.datawiz.io/login/')
-        wait = WebDriverWait(driver, 15, 0.3)
-        wait.until(ec.visibility_of_element_located((By.ID, "id_auth-username")))
-        login = driver.find_element(By.ID, "id_auth-username")
-        login.send_keys('andrew.masyuk@datawiz.io')
-        password = driver.find_element(By.ID, "id_auth-password")
-        password.send_keys('Qweasd2zxc')
-        button = driver.find_element(By.XPATH, "//button[@class='btn btn-primary btn-block']")
-        button.click()
+    # def login(self):
+    #     driver_service = Service(ChromeDriverManager().install())
+    #     driver = webdriver.Chrome(service=driver_service)
+    #     driver.maximize_window()
+    #     driver.get('https://bes.datawiz.io/login/')
+    #     wait = WebDriverWait(driver, 15, 0.3)
+    #     wait.until(ec.visibility_of_element_located((By.ID, "id_auth-username")))
+    #     login = driver.find_element(By.ID, "id_auth-username")
+    #     login.send_keys('andrew.masyuk@datawiz.io')
+    #     password = driver.find_element(By.ID, "id_auth-password")
+    #     password.send_keys('Qweasd2zxc')
+    #     button = driver.find_element(By.XPATH, "//button[@class='btn btn-primary btn-block']")
+    #     button.click()
 
     def test_three(self):
         # self.login()
@@ -46,17 +46,66 @@ class TestTwo:
         # last_ninety_days_button = driver.find_element(By.XPATH, '//div[@class="ant-tabs-tab"][3]')
         last_ninety_days_button.click()
 
-        plan_by_sells_qty_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper'][1]")
+        plan_by_sells_qty_button = wait.until(ec.presence_of_element_located((By.XPATH, "//label[@class='ant-radio-button-wrapper'][1]")))
         plan_by_sells_qty_button.click()
+        time.sleep(1)
 
         plan_by_profit_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper'][2]")
         plan_by_profit_button.click()
+        time.sleep(1)
+
+        plan_by_seles_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper']")
+        plan_by_seles_button.click()
+        time.sleep(1)
+
+
+        last_seven_days_button = wait.until(ec.presence_of_element_located((By.XPATH, '//div[@class="ant-tabs-tab"][2]')))
+        last_seven_days_button.click()
+
+        plan_by_sells_qty_button = wait.until(ec.presence_of_element_located((By.XPATH, "//label[@class='ant-radio-button-wrapper'][1]")))
+        plan_by_sells_qty_button.click()
+        time.sleep(1)
+
+        plan_by_profit_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper'][2]")
+        plan_by_profit_button.click()
+        time.sleep(1)
+
+        plan_by_seles_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper']")
+        plan_by_seles_button.click()
+        time.sleep(1)
+
+
+        last_thirty_days_button = wait.until(ec.presence_of_element_located((By.XPATH, '//div[@class="ant-tabs-tab"][2]')))
+        last_thirty_days_button.click()
+
+        plan_by_sells_qty_button = wait.until(ec.presence_of_element_located((By.XPATH, "//label[@class='ant-radio-button-wrapper'][1]")))
+        plan_by_sells_qty_button.click()
+        time.sleep(1)
+
+        plan_by_profit_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper'][2]")
+        plan_by_profit_button.click()
+        time.sleep(1)
+
+        plan_by_seles_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper']")
+        plan_by_seles_button.click()
+        time.sleep(1)
+
+
+        last_current_date_button = wait.until(ec.presence_of_element_located((By.XPATH, '//div[@class="ant-tabs-tab"][1]')))
+        last_current_date_button.click()
+
+        plan_by_sells_qty_button = wait.until(ec.presence_of_element_located((By.XPATH, "//label[@class='ant-radio-button-wrapper'][1]")))
+        plan_by_sells_qty_button.click()
+        time.sleep(1)
+
+        plan_by_profit_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper'][2]")
+        plan_by_profit_button.click()
+        time.sleep(1)
 
         plan_by_seles_button = driver.find_element(By.XPATH, "//label[@class='ant-radio-button-wrapper']")
         plan_by_seles_button.click()
 
         time.sleep(5)
-
 
 
 
